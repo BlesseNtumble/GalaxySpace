1ff45dfdf406c8c71e5a92654328f88a765da85c
BlesseNtumble <vitalya_migun@mail.ru>
2019-08-05 13:12:46 +0900

    Added burn time for logs and planks Bar.C

--- src/main/java/galaxyspace/systems/BarnardsSystem/core/events/BREventHandler.java
+++ src/main/java/galaxyspace/systems/BarnardsSystem/core/events/BREventHandler.java
@@ -3,8 +3,10 @@ package galaxyspace.systems.BarnardsSystem.core.events;
 import galaxyspace.systems.BarnardsSystem.core.registers.BRBlocks;
 import galaxyspace.systems.BarnardsSystem.planets.barnarda_c.blocks.Barnarda_C_Blocks;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -21,5 +23,20 @@ public class BREventHandler {
 			world.setBlockState(event.getPos(), BRBlocks.BARNARDA_C_FARMLAND.getDefaultState(), 3);
 			event.setResult(Result.ALLOW);
 		}		
+	}
+	
+	@SubscribeEvent
+	public void furnanceFuel(FurnaceFuelBurnTimeEvent e)
+	{
+		ItemStack stack = e.getItemStack();
+		if(stack.isItemEqual(new ItemStack(BRBlocks.BARNARDA_C_BLOCKS, 1, 6)))
+		{
+			e.setBurnTime(180);
+		}
+		
+		if(stack.isItemEqual(new ItemStack(BRBlocks.BARNARDA_C_VIOLET_LOG, 1, 0)))
+		{
+			e.setBurnTime(-1);
+		}
 	}
 }
