7517265caad6ad9518faf445ed97001ee4fd54fe
BlesseNtumble <vitalya_migun@mail.ru>
2020-04-26 09:56:15 +0900

    Some fixs for SpaceSuit overlay

--- src/main/java/galaxyspace/core/client/gui/overlay/OverlaySpaceSuit.java
+++ src/main/java/galaxyspace/core/client/gui/overlay/OverlaySpaceSuit.java
@@ -76,6 +76,15 @@ public class OverlaySpaceSuit extends Overlay {
 		int xOffset = -35, yOffset = scaled.getScaledHeight() - 140;
 		final ResourceLocation guiTexture = new ResourceLocation(AsmodeusCore.ASSET_PREFIX, "textures/gui/gui.png");
 		
+		if(GSConfigCore.spacesuit_pos.equals("up") || GSConfigCore.spacesuit_pos.equals("top")) 
+			yOffset = 70;
+		
+		if(GSConfigCore.spacesuit_pos.equals("center")) 
+			yOffset = scaled.getScaledHeight() / 2 + 20;
+		
+		if(GSConfigCore.spacesuit_pos.equals("down") || GSConfigCore.spacesuit_pos.equals("bottom")) 
+			yOffset = scaled.getScaledHeight() - 140;
+		
 		if(hasSuit) {
 			//mc.entityRenderer.setupOverlayRendering();
 			GlStateManager.enableBlend();
@@ -134,19 +143,7 @@ public class OverlaySpaceSuit extends Overlay {
 				if(player.getItemStackFromSlot(EntityEquipmentSlot.FEET).equals(stack)) {
 					if(stack.getTagCompound().getBoolean(ItemSpaceSuit.suit_buttons[3])) { enable = true;}
 				};
-								
-				
-				
-				if(GSConfigCore.spacesuit_pos.equals("up") || GSConfigCore.spacesuit_pos.equals("top")) 
-					yOffset = 70;
-				
-				if(GSConfigCore.spacesuit_pos.equals("center")) 
-					yOffset = scaled.getScaledHeight() / 2 + 20;
-				
-				if(GSConfigCore.spacesuit_pos.equals("down") || GSConfigCore.spacesuit_pos.equals("bottom")) 
-					yOffset = scaled.getScaledHeight() - 140;
-				
-				
+						
 				String key = keys[3 - player.inventory.armorInventory.indexOf(stack)];
 				
 				
